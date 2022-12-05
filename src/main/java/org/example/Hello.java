bd9877d4dc2cfa7adc4c4d82e9815539e86bdfab
sergyops <70977028+sergyops@users.noreply.github.com>
2022-12-05 12:09:49 +0100

    Initial commit

---
+++ src/main/java/org/example/Hello.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Hello {
+
+    public void helloWorld(){
+        System.out.println("Hello World :)");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+       Hello hello = (Hello) context.getBean("hello");
+
+       hello.helloWorld();
+    }
+}
